8291d83c406bac179081c04ef2a9d7cd9ff0dd6b
Leo <2020302191274@whu.edu.cn>
2022-09-27 17:43:42 +0800

    I promise to run OK

--- main/main.c
+++ main/main.c
@@ -1,4 +1,4 @@
-#define BASE_ON_ESP
+#define BASE_ON_ESP32
 #include <stdio.h>
 #include "string.h"
 #include "freertos/FreeRTOS.h"
@@ -26,9 +26,9 @@
 #define MAX_U 4.99f
 #define MAX_I 0.13f
 #define MAX_V 700.0f
-#define MIN_V 70.0f//只在速度足够大才控制电流
-#define PARA_ENCODER 14.28f //100*2pi/44 11线编码器
-#define DELAY_KHZ 0.1f
+#define MIN_V 70//只在速度足够大才控制电流
+#define PARA_ENCODER 14.28f //(DELAY_HZ)100*2pi/44 11线编码器
+#define DELAY_MS 10
 #define SAMPLING_R 0.000634921f
 #define PWM_HZ 40000
 
@@ -39,26 +39,24 @@ static void SpeedThread_(void *arg);
 QueueHandle_t g_current_queue;//电流采样值的序列
 QueueHandle_t g_current_t_queue;//目标电流值
 
+//ENCODER相关配置
 int32_t g_adc_offset;//采样偏差
 rotary_encoder_t *g_encoder = NULL;//rotary encoder handle
 
 #define ___EspAdc1ConfigWidth adc1_config_width
 #define ___EspAdc1ConfigChannelAtten adc1_config_channel_atten
 #define ___EspAdc1GetRaw adc1_get_raw
-#define ___EspRotaryEncoderDefaultConfig ROTARY_ENCODER_DEFAULT_CONFIG
 #define ___EspRotaryEncoderNewEc11 rotary_encoder_new_ec11
-
 #define ___EspMcpwmGpioInit mcpwm_gpio_init
 #define ___EspMcpwmInit mcpwm_init
-
 #define ___EspMcpwmIsrRegister mcpwm_isr_register
 #define ___EspMcpwmSetDuty mcpwm_set_duty
+
 inline void PysbMotorInit(){
 /*     ESP_ERROR_CHECK(gpio_reset_pin(GPIO_NUM_21));
     ESP_ERROR_CHECK(gpio_set_direction(GPIO_NUM_21, GPIO_MODE_OUTPUT)); */
-
+    #ifdef BASE_ON_ESP32
     //配置电流采样adc接口
-    #ifdef BASE_ON_ESP
     ESP_ERROR_CHECK(___EspAdc1ConfigWidth(ADC_WIDTH_BIT_12));
     ESP_ERROR_CHECK(___EspAdc1ConfigChannelAtten(ADC1_CHANNEL_7, ADC_ATTEN_DB_11));
     ///测定基础电流,取50次取平均值足够了
@@ -70,7 +68,7 @@ inline void PysbMotorInit(){
 
     //配置电机编码器
     uint32_t pcnt_unit = 0;
-    rotary_encoder_config_t config = ___EspRotaryEncoderDefaultConfig((rotary_encoder_dev_t)pcnt_unit, ROTARY_ENCODER_PINA, ROTARY_ENCODER_PINB);
+    rotary_encoder_config_t config = ROTARY_ENCODER_DEFAULT_CONFIG((rotary_encoder_dev_t)pcnt_unit, ROTARY_ENCODER_PINA, ROTARY_ENCODER_PINB);
     ESP_ERROR_CHECK(___EspRotaryEncoderNewEc11(&config, &g_encoder));
     ESP_ERROR_CHECK(g_encoder->set_glitch_filter(g_encoder, 1));
     ESP_ERROR_CHECK(g_encoder->start(g_encoder));
@@ -79,10 +77,12 @@ inline void PysbMotorInit(){
     ESP_ERROR_CHECK(___EspMcpwmGpioInit(MCPWM_UNIT_0, MCPWM0B, MCPWM0B_PIN));
 
     //设置为PWM更新模式为延迟
-    MCPWM0.operators[0].gen_stmp_cfg.gen_a_upmethod = 1;
-    MCPWM0.operators[0].gen_stmp_cfg.gen_b_upmethod = 1;
-    MCPWM0.operators[0].gen_stmp_cfg.gen_a_shdw_full = 1;
-    MCPWM0.operators[0].gen_stmp_cfg.gen_b_shdw_full = 1;
+    {
+        MCPWM0.operators[0].gen_stmp_cfg.gen_a_upmethod = 1;
+        MCPWM0.operators[0].gen_stmp_cfg.gen_b_upmethod = 1;
+        MCPWM0.operators[0].gen_stmp_cfg.gen_a_shdw_full = 1;
+        MCPWM0.operators[0].gen_stmp_cfg.gen_b_shdw_full = 1;
+    }
 
     mcpwm_config_t pwm_config;
     {
@@ -92,8 +92,8 @@ inline void PysbMotorInit(){
         pwm_config.counter_mode =  MCPWM_UP_DOWN_COUNTER;
         pwm_config.duty_mode = MCPWM_DUTY_MODE_1; 
     }
-    ESP_ERROR_CHECK(___EspMcpwmInit(MCPWM_UNIT_0, MCPWM_TIMER_0, &pwm_config));
-    #endif
+    ESP_ERROR_CHECK(___EspMcpwmInit(MCPWM_UNIT_0, MCPWM_TIMER_0, &pwm_config)); 
+    #endif   
 }
 
 void app_main(void)
@@ -259,6 +259,6 @@ static void SpeedThread_(void *arg)
 
         xQueueSend(g_current_t_queue, &i_target, portMAX_DELAY);//发送目标电流
 
-        vTaskDelayUntil(&xLastWakeTime, 1 / DELAY_KHZ*portTICK_PERIOD_MS);
+        vTaskDelayUntil(&xLastWakeTime, DELAY_MS/ portTICK_PERIOD_MS);
     }
 }
